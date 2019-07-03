7513c70bfe846152553cb6bfa190fdf4a7eaaa73
araujobsd <msa@bitmark.com>
2019-07-03 11:40:23 +0800

    - Move back build+ flag and add a blank line after its declaration

--- utils/findcommand.go
+++ utils/findcommand.go
@@ -1,3 +1,5 @@
+// +build !windows
+
 // SPDX-License-Identifier: BSD-2-Clause
 /*-
  * Copyright 2019 by Marcelo Araujo <araujo@FreeBSD.org>
@@ -25,7 +27,6 @@
  *
  */
 
-// +build !windows
 package utils
 
 import (
